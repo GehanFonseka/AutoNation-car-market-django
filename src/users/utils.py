b2a2d16faa041bc95dacbe86849d715074d070e4
GehanFonseka <gehanfonseka@gmail.com>
2024-11-24 14:58:43 +0530

    initial commit

---
+++ src/users/utils.py
@@ -0,0 +1,4 @@
+def user_directory_path(instance, filename):
+    return 'user {0}/{1}'.format(instance.user.id, filename)
+     
+ 
---
+++ src/users/views.py
@@ -0,0 +1,5 @@
+from http.client import HTTPResponse
+from django.shortcuts import render
+
+def login_view(request):
+    return HTTPResponse("Login View :)")
