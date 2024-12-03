94547af8bc831e37e5d47d0eb1531b095ffd94b6
GehanFonseka <gehanfonseka@gmail.com>
2024-12-03 21:51:32 +0530

    update .gitignore

--- src/users/forms.py
+++ src/users/forms.py
@@ -6,12 +6,27 @@ from .widgets import CustomPictureImageFieldWidget
 from .models import Location, Profile
 
 
+
 class UserForm(forms.ModelForm):
     username = forms.CharField(disabled=True)
 
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email')
+
+    def clean_password1(self):
+        # Skip unwanted validations for password (if there is a password field)
+        password = self.cleaned_data.get('password1')
+        if password and len(password) < 8:
+            raise forms.ValidationError("Your password must contain at least 8 characters.")
+        return password
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("The two password fields must match.")
+        return password2
 
 
 class ProfileForm(forms.ModelForm):
