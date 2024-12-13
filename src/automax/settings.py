059dc4015e51508771bc56e1b1eeb0f7829568ec
GehanFonseka <gehanfonseka@gmail.com>
2024-12-13 17:10:27 +0530

    Update settings.py

--- src/automax/settings.py
+++ src/automax/settings.py
@@ -76,7 +76,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            
+             BASE_DIR / 'templates', 
             ],
         'APP_DIRS': True,
         'OPTIONS': {
